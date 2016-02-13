0c256b16c0c7ca5fa1d29ec8d4bd491ae14d1028
DanielChiang <ssw1200@gmail.com>
2016-02-14 03:17:23 +0800

    Subset done

--- Subset.java
+++ Subset.java
@@ -1,5 +1,18 @@
-public class Subset {
-   public static void main(String[] args) {
+import edu.princeton.cs.algs4.StdIn;
 
-   }
+public class Subset {
+    public static void main(String[] args) {
+        int limit = Integer.parseInt(args[0]);
+
+        RandomizedQueue<String> rq = new RandomizedQueue<String>();
+
+        while (!StdIn.isEmpty()) {
+            String s = StdIn.readString();
+            rq.enqueue(s);
+        }
+
+        for (int i = 0; i < limit; i++) {
+            System.out.println(rq.dequeue());
+        }
+    }
 }
